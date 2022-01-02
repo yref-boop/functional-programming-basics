4febbd253475cc5633a21dfd5f5373e9158db333
yago <regofdez.yago@gmail.com>
2022-01-02 20:22:17 +0100

    p9 optional, to be finished

---
+++ p9/fact.ml
@@ -0,0 +1,14 @@
+let rec fact = function
+	0 -> 1
+	| n -> n * fact (n-1)
+
+let size = Array.length Sys.argv
+
+let _ =
+	if size <> 2 then print_endline "error"
+	else 
+        print_endline(string_of_int(fact(
+            try int_of_string(Array.get Sys.argv(1))
+            with _ -> 0)))
+;;
+
