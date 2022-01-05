5f7a90a9121e2932ce0b3432569027b9aafc9dae
yago <regofdez.yago@gmail.com>
2022-01-05 14:59:54 +0100

    fixed small error p9

--- p9/fact.ml
+++ p9/fact.ml
@@ -8,7 +8,7 @@ let _ =
 	if size <> 2 then print_endline "error"
 	else 
         try
-        print_endline(string_of_int(fact(int_of_string(Sys.argv(1)))))
+        print_endline(string_of_int(fact(int_of_string(Array.get Sys.argv(1)))))
         with
             Failure "int_of_string" -> print_endline "Please, use a number as input"
     | Stack_overflow -> print_endline "Please, use only positive integers"
