b361994696caf84a980a44196114f1aee96e582f
yago <regofdez.yago@gmail.com>
2022-01-08 21:09:57 +0100

    finished p11

--- p11/shortest.ml
+++ p11/shortest.ml
@@ -1,36 +1,3 @@
-(*tour : int -> int -> (int * int) -> (int * int) -> (int * int) list*)
-
-(*check instead of the row, the 8 posibilities: do not go into already visited, do not go out*)
-
-(*store all visited on list "visited"*)
-
-let nmem l e =
-        not (List.mem e l)
-;;
-
-let in_board m n (x,y) =
-        x >= 1 && x <= m && y >= 1 && y <= n;;
-
-let legal_moves n m visited (x,y) =
-        let all_moves = [x+1, y+2; x-1, y+2; x+1, y-2; x-1, y-2;
-                        x+2, y+1; x+2, y-1; x-2, y+1; x-2,y-1]
-        in List.filter (nmem visited) (List.filter(in_board n m) all_moves)
-;;
-
-let rec tour n m (xi,yi) (xf,yf)=
-    if (m < 0) || (n < 0) || (not(in_board m n (x,y)))
-        then raise Invalid_argument "tour"
-    else let rec aux solution = function
-            [] -> raise Not_found
-            | h::t -> if (h = y) then List.rev (h::solution)
-                else try aux (h::solution) (legal_moves m n h (h::solution))
-                     with Not_found -> aux solution t
-        in if x = y then [x]
-    else aux [x] (legal_moves m n x [x])
-;;
-
-
-
 let nmem l e =
         not (List.mem e l)
 ;;
@@ -46,7 +13,7 @@ let legal_moves m n visited (x,y) =
 ;;
 
 
-let tour m n (xi,yi) (xf,yf) =
+let shortest_tour m n (xi,yi) (xf,yf) =
         if (m < 0) || (n < 0) || (not(in_board m n (xf,yf)))
             then raise (Invalid_argument "tour")
         else 
